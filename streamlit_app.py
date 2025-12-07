bd7e5dd10eacd955e2f8b943bc4d68b0f925b6d0
saphaluli <timetimestwo@gmail.com>
2025-12-07 17:30:11 +0000

    Started adding text, adding code should be copy paste task now

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,5 +19,21 @@ st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 
+st.title('Introduction')
+st.subheader('Cardiovascular disease and research aim')
+#first paraphraph
+st.write('Cardiovascular disease (CVD) is the leading cause of death globally, accounting for' \
+' approximately 38\% of global deaths (1, WHO (2025). The term CVD includes a range of diseases, such as stroke,' \
+' heart attack or peripheral arterial disease. CVD is often caused by a combination of unhealthy' \
+' lifestyle factors. Early detection of patients with high risk of developing CVD is therefore very' \
+' important, as modifying lifestyle factors can greatly decrease the risk of developing a disease under the CVD umbrella.'
+)
+#Second paragraph - research aim/description of model
+st.write('Through the use of data analysis and different AI/machine learning approaches, this project' \
+'aims to quantify which baseline factors are associated with earlier developments of CVD, and whether this ' \
+' risk can be predicted accurately through these factors.')
+
+st.title('References')
+st.write(' 1. World Health Organisation (2025), "Cardiovascular diseases (CVDs)", pls check how to do manual reference https://www.who.int/news-room/fact-sheets/detail/cardiovascular-diseases-(cvds)')
 
 
