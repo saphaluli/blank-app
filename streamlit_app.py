f5bf7fe0e078b7d45ada0ee7b35696dfafd5f1a4
saphaluli <timetimestwo@gmail.com>
2025-12-07 12:53:47 +0000

    Fixed sklearn not imported, make sure to use pip install

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,23 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
+import sklearn
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDisplay
+from sklearn.metrics import roc_curve, auc
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_predict
+from sklearn.metrics import f1_score, precision_score, recall_score, roc_auc_score, roc_curve, RocCurveDisplay, precision_recall_curve
 
-st.title("🎈 My new Streamlit app")
+st.title("🤍 Predicting onset of Cardiovascular disease from data?")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
 
 
